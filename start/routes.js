9d3f7892378c6dac7bda38bb4b1c051227cfcb13
Vinicius <vinicius.moura@convergenceworks.com.br>
2019-10-31 15:41:00 -0300

    initial commit

---
+++ start/routes.js
@@ -0,0 +1,28 @@
+'use strict'
+const { rastro } = require('rastrojs');
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| Http routes are entry points to your web application. You can create
+| routes for different URLs and bind Controller actions to them.
+|
+| A complete guide on routing is available here.
+| http://adonisjs.com/docs/4.1/routing
+|
+*/
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use('Route')
+
+
+Route.get('/', () => {
+  return { greeting: 'Hello world in JSON' }
+})
+Route.post('/users', 'UserController.create')
+Route.post('/sessions', 'SessionController.create')
+Route.get('track/:packageId', ({ params }) => {
+
+  return rastro.track(params.packageId.split('&'))
+}).middleware('auth')
